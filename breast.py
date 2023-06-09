18bab130b1e42716518efe111fdb4d7a0d2beff3
mimierivha <85709852+mimierivha@users.noreply.github.com>
2023-06-09 11:27:58 +0200

    Update breast.py

--- breast.py
+++ breast.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 
 # Load the saved model
-model_path = 'C:/Users/dell/Desktop/mimi/dissertation.h5'
+model_path = 'C:/Users/dell/Desktop/mimi/diss.h5'
 model = tf.keras.models.load_model(model_path)
 
 # Define a function to preprocess the image and make a prediction
@@ -37,9 +37,14 @@ def predict(image):
 
     # Return the prediction as a probability score
     return prediction[0][0]
+
 # Define the layout of the app
-st.title('Breast Cancer Classification')
-st.write('Upload an image of a breast to classify it as benign or malignant')
+st.title('Breast Cancer Histopathology Image Classification')
+st.write('This app uses a pre-trained machine learning model to classify breast cancer images as either benign or malignant. Upload an image of a biopsy to get started.')
+
+# Add a slider to adjust the prediction threshold
+threshold = st.slider('Prediction threshold:', 0.0, 1.0, 0.5, 0.05)
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
@@ -56,7 +61,32 @@ if uploaded_file is not None:
         prediction = predict(image)
 
         # Display the prediction
-        if prediction >0.5:
+        if prediction > threshold:
             st.write('The image is malignant')
         else :
             st.write('The image is benign')
+
+        # Add a visual representation of the prediction
+        prob_malignant = round(prediction*100,2)
+        prob_benign = round((1-prediction)*100,2)
+        st.write(f'Probability of malignant tumor: {prob_malignant}%')
+        st.write(f'Probability of benign tumor: {prob_benign}%')
+        st.bar_chart({'Malignant': prob_malignant, 'Benign': prob_benign})
+
+
+
+        # Add a feedback mechanism
+        st.write('Was the prediction accurate? Please leave your feedback below.')
+        feedback = st.text_input('Feedback:')
+        if st.button('Submit'):
+            # Save the feedback to a file or database
+            st.write('Thank you for your feedback!')
+
+        # Add a description of the model and its accuracy
+        st.write('The model used in this app was trained on a dataset ofbreast cancer histopathology images to classify the images as either benign or malignant. The model has an accuracy of X% on the test set. The model uses a convolutional neural network (CNN) architecture to extract features from the images and make a prediction. The CNN consists of X layers and was trained using the Adam optimizer with a learning rate of X.')
+
+        # Add an explanation of how to interpret the prediction
+        st.write(f'The prediction threshold is set to {threshold}. If the model predicts a probability of malignancy greater than this threshold, the prediction is "malignant". If the predicted probability is less than the threshold, the prediction is "benign".')
+
+        # Add a brief conclusion to the app
+        st.write('Thank you for using our breast cancer histopathology image classification app. If you have any feedback or questions, please feel free to contact us.')
